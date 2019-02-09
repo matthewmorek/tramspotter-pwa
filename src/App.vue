96123c14feb7cfc33de8ddd82bec9388110e4e0c
Matthew Morek <matthew@madbit.co>
2019-02-09 18:11:24 +0000

    🚧 Continue scaffolding the app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AppHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import AppHeader from "./components/AppHeader.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    AppHeader
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "~reset-css/sass/reset";
+@import "~tachyons-sass/tachyons.scss";
+
+html,
+body {
+  font-family: "Muli", -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="app-header ml-auto mr-auto bg-black">
+    <h1 class="app-title gold">Tram-spotter</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.app-title {
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  font-size: 0.875rem;
+  font-weight: 700;
+  line-height: 1.2;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  text-align: center;
+}
+</style>
