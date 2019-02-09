f0d74617f36c15d71e445d6a9b7d00cd08bb3474
Matthew Morek <matthew@madbit.co>
2019-02-09 18:32:08 +0000

    🚧 Add a WelcomeScreen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <AppHeader />
+    <WelcomeScreen />
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import WelcomeScreen from "./components/WelcomeScreen.vue";
+
 export default {
   name: "app",
   components: {
-    AppHeader
+    AppHeader,
+    WelcomeScreen
   }
 };
 </script>
@@ -22,5 +26,33 @@ html,
 body {
   font-family: "Muli", -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, sans-serif;
+  line-height: 1.4rem;
+  font-size: 1.125rem;
+  font-weight: 400;
+}
+
+p {
+  margin-top: 0;
+  margin-bottom: 1em;
+
+  & + p {
+    margin-top: 1em;
+  }
+}
+
+ul,
+ol {
+  padding-left: 1.5rem;
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+
+ul {
+  list-style-type: square;
+}
+
+img {
+  max-width: 100%;
+  display: block;
 }
 </style>
---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,42 @@
+<template>
+  <article class="app-content">
+    <p>
+      This app will allow you to find out nearest Metrolink tram stop and see
+      upcoming departures based on your current location in real time.
+    </p>
+    <p>
+      It requires access to your device location services in order to work
+      accurately.
+    </p>
+    <p>
+      <button @click="enableGeolocation">Locate a nearest tram stop</button>
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "WelcomeScreen",
+
+  data() {
+    return {};
+  },
+  methods: {
+    enableGeolocation: function() {
+      this.$getLocation({
+        enableHighAccuracy: true,
+        timeout: Infinity,
+        maximumAge: 86400
+      }).then(coordinates => {
+        console.log(coordinates);
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.app-content {
+  padding: 1rem;
+}
+</style>
