4926b33d0891c726f1d11ca92405aeb253dcac28
Matthew Morek <matthew@madbit.co>
2019-05-10 00:16:00 +0100

    🚧 Add sample data structure

---
+++ _data-structure.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+const compiled = {
+  arrivals: [
+    {
+      carriage: 'Single', // Single/Double
+      destination: 'Rochdale Town Centre',
+      status: 'Due', // Due, Departing, Arriving
+      wait: '22' // minutes
+    },
+    {
+      carriage: 'Single', // Single/Double
+      destination: 'Rochdale Town Centre',
+      status: 'Due', // Due, Departing, Arriving
+      wait: '14' // minutes
+    },
+    {
+      carriage: 'Single', // Single/Double
+      destination: 'Rochdale Town Centre',
+      status: 'Due', // Due, Departing, Arriving
+      wait: '10' // minutes
+    },
+    {
+      carriage: 'Single', // Single/Double
+      destination: 'Rochdale Town Centre',
+      status: 'Due', // Due, Departing, Arriving
+      wait: '8' // minutes
+    }
+  ],
+  distance: 1234,
+  lastUpdated: '2019-05-09T22:16:25Z',
+  line: 'South Manchester',
+  messageBoard:
+    'On Sunday 12th  May Manchester United welcome Cardiff to Old Trafford. Kick off is 15:00 and services are expected to be busier than usual please allow extra time to make your journey.',
+  stationLocation: 'East Didsbury',
+  direction: 'Incoming'
+};
