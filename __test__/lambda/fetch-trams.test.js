b11bc979d34e959f776ab518467858763d992abc
Kyle Welsby <kyle@mekyle.com>
2019-06-19 15:58:52 +0100

    Unit-test example

---
+++ __test__/lambda/fetch-trams.test.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import nock from 'nock';
+import { handler } from '../../src/lambda/fetch-trams';
+
+describe('Manchester Metrolink', () => {
+  it('returns the metrolink data', async () => {
+    /*
+     enabling nock.recorder.rec() will show what output you'll need to put below
+     see https://github.com/nock/nock#recording
+     */
+    // nock.recorder.rec()
+    nock('https://api.tfgm.com')
+      .get('/odata/Metrolinks')
+      .query(true)
+      .replyWithFile(
+        200,
+        path.join(__dirname, '../__fixtures__/metrolinks.json')
+      );
+
+    const data = await handler(null, null);
+    const json = JSON.parse(data.body);
+    expect(data.statusCode).toEqual(200);
+    expect(data.headers['Content-Type']).toEqual('application/json');
+    expect(json[0]).toEqual(
+      expect.objectContaining({
+        id: 695,
+        line: 'Airport'
+      })
+    );
+  });
+});
--- src/lambda/fetch-trams.js
+++ src/lambda/fetch-trams.js
@@ -1,51 +1,18 @@
 import axios from 'axios';
+import camelcaseKeys from 'camelcase-keys';
 
-function toCamel(o) {
-  var newO, origKey, newKey, value;
-  if (o instanceof Array) {
-    return o.map(function(value) {
-      if (typeof value === 'object') {
-        value = toCamel(value);
-      }
-      return value;
-    });
-  } else {
-    newO = {};
-    for (origKey in o) {
-      if (o.hasOwnProperty(origKey)) {
-        newKey = (
-          origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey
-        ).toString();
-        value = o[origKey];
-        if (
-          value instanceof Array ||
-          (value !== null && value.constructor === Object)
-        ) {
-          value = toCamel(value);
-        }
-        newO[newKey] = value;
-      }
+export async function handler() {
+  const response = await axios.get('https://api.tfgm.com/odata/Metrolinks', {
+    params: { $select: 'Id,AtcoCode,StationLocation' },
+    headers: {
+      'Ocp-Apim-Subscription-Key': 'aee97b95e83747b3997f2131137abcfe'
     }
-  }
-  return newO;
-}
-
-export function handler(event, context, callback) {
-  axios
-    .get('https://api.tfgm.com/odata/Metrolinks', {
-      params: { $select: 'Id,AtcoCode,StationLocation' },
-      headers: {
-        'Ocp-Apim-Subscription-Key': 'aee97b95e83747b3997f2131137abcfe',
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
-    .then(response =>
-      callback(null, {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(toCamel(response.data.value))
-      })
-    );
+  });
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(camelcaseKeys(response.data.value))
+  };
 }
