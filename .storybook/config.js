320ca53c364b8dad6fed079fdf6a486f7cc24350
Matthew Morek <matthew@madbit.co>
2019-09-18 12:40:13 +0100

    📦 Add Storybook Viewports addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from '@storybook/vue';
+import { addParameters } from '@storybook/vue';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import '../src/styles/global.css';
 
 const req = require.context('../src/stories', true, /.stories.js$/);
@@ -6,5 +8,12 @@ const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphone6',
+  },
+});
 
 configure(loadStories, module);
