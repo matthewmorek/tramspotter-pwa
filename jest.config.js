f33259e75ad121e32ba3b7d56723189f1a661c8c
Matthew Morek <matthew@madbit.co>
2019-09-19 15:01:48 +0100

    🔧 Add Jest support for transforming SVGs

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,11 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
     '^.+\\.css$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
     '^.+\\.vue$': 'vue-jest',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.svg$': '<rootDir>/src/utilities/svgTransform.js'
   },
   transformIgnorePatterns: ['/node_modules/(?!(@storybook/.*\\.vue$))'],
   moduleNameMapper: {
