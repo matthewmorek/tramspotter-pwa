8d443e113b294dbe2486f1db584b7bb9ba50f7a7
Matthew Morek <matthew@madbit.co>
2019-09-19 16:07:04 +0100

    🔥 Remove Storybook

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ['@babel/plugin-transform-runtime', 'macros'],
+  plugins: ['@babel/plugin-transform-runtime'],
   presets: [
     // '@vue/app',
     ['@babel/preset-env', { targets: { node: true } }]
--- src/stories/BouncingBalls.stories.js
+++
@@ -1,8 +0,0 @@
-// import Vue from 'vue';
-import { storiesOf } from '@storybook/vue';
-import BouncingBalls from '../components/BouncingBalls';
-
-storiesOf('Elements|BouncingBalls', module).add('default', () => ({
-  components: { BouncingBalls },
-  template: '<bouncing-balls />'
-}));
