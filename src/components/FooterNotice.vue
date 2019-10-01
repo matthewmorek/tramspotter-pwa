e711bb7d4345da22d7ff29653c1ede167591324e
Matthew Morek <matthew@madbit.co>
2019-10-01 17:20:44 +0100

    🚧 Add FooterNotice abstract component

---
+++ src/components/FooterNotice.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-footer--notice">
+    <div class="app-footer--notice-icon">
+      <slot name="icon"></slot>
+    </div>
+    <div class="app-footer--notice-text">
+      <slot name="text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss">
+.app-footer--notice {
+  display: flex;
+  align-items: center;
+  margin-top: 1.5rem;
+
+  &-text {
+    font-size: 0.875rem;
+    padding-left: 0.75rem;
+  }
+
+  &--icon {
+    .icon {
+      fill: var(--icon-contrast);
+    }
+  }
+}
+</style>
