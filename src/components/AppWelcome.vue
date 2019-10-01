70c9890507e47809deb17fd898aa74f0e8cb3867
Matthew Morek <matthew@madbit.co>
2019-10-01 17:11:54 +0100

    🚧 Add AppWelcome component

--- src/components/AppWelcome.vue
+++ src/components/AppWelcome.vue
@@ -1,8 +1,8 @@
 <template>
-  <main class="app-main">
-    <AppIcon />
-    <h1 class="app-title">Tramspotter</h1>
-    <p class="app-description">
+  <main class="app-welcome">
+    <app-icon />
+    <h1 class="app-welcome--title">Tramspotter</h1>
+    <p class="app-welcome--info">
       Find your nearest Metrolink tram stop and check it for live departures.
     </p>
   </main>
@@ -18,21 +18,21 @@ export default {
 </script>
 
 <style lang="postcss">
-.app-main {
+.app-welcome {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
 
-.app-title {
+.app-welcome--title {
   text-transform: uppercase;
   font-weight: 700;
   text-align: center;
   margin-bottom: 1rem;
 }
 
-.app-description {
+.app-welcome--info {
   font-size: 1.125rem;
   text-align: center;
 }
