e2f1ecca34ecf6aaeb1fe5dcb9bdfab5b64fcab7
Matthew Morek <matthew@madbit.co>
2019-11-09 15:02:41 +0000

    🚧 Update AppHeader
    - Begin adding props;
    - Reformat codebase;
    - Update styles;

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,11 @@
 <template>
   <header class="app-header">
     <div>
-      <AppIcon class="app-icon" />
+      <app-icon class="app-icon" />
     </div>
     <div>
-      <h2 class="app-header--name">{{ stopLocation }}</h2>
+      <h2 class="app-header--stop-name">{{ stopLocation }}</h2>
+      <p class="app-header--stop-distance">{{ distanceToStop }} mls away</p>
     </div>
   </header>
 </template>
@@ -19,7 +20,13 @@ export default {
   props: {
     stopLocation: {
       type: String,
-      required: true
+      required: true,
+      default: null
+    },
+    distanceToStop: {
+      type: Number,
+      required: true,
+      default: null
     }
   }
 };
@@ -30,8 +37,13 @@ export default {
   display: flex;
   align-items: center;
 }
-.app-header--name {
+.app-header--stop-name {
   font-size: 1.25rem;
   font-weight: 700;
+  line-height: 1.4;
+}
+.app-header--stop-distance {
+  font-size: 0.875rem;
+  font-weight: 400;
 }
 </style>
