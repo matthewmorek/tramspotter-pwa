084691a4295514234d5cf388a00e9ad7e404d9f1
Matthew Morek <matthew@madbit.co>
2019-11-10 00:32:58 +0000

    🔧 Adjust Storybook config to make SVGs work

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
 
 module.exports = async ({ config, mode }) => {
+  // filter out SVGs from being in conflict with webpack's file-loader
+  config.module.rules = config.module.rules.map(rule => {
+    if (
+      String(rule.test) ===
+      String(
+        /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+      )
+    ) {
+      return {
+        ...rule,
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+      };
+    }
+    return rule;
+  });
+
   config.module.rules.push({
     test: /\.postcss$/,
     use: ['vue-style-loader', 'css-loader', 'postcss-loader']
@@ -9,6 +25,11 @@ module.exports = async ({ config, mode }) => {
   config.module.rules.push({
     test: /\.css$/,
     use: ['postcss-loader']
+  });
+
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: [{ loader: 'vue-svg-loader' }]
   });
 
   return config;
