ad640d183711dffef4bbad4668045e54b5a2cd79
Matthew Morek <matthew@madbit.co>
2019-11-19 17:57:05 +0000

    🔧 Update Jest configuration to properly parse markup
    - properly parse SVGs inside Jest snapshots
    
    [ch42]

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.svg$': '<rootDir>/.storybook/svg-transform.js',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub'
   },
   transformIgnorePatterns: ['/node_modules/(?!(@storybook/.*\\.vue$))'],
-  moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '^.+\\.(css)$': '<rootDir>/.storybook/css-stub.js'
-  }
+  moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'node']
 };
