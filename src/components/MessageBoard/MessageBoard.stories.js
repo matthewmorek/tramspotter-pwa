45a9314818dd260b7cf18c616d20f744ede4e0e0
Matthew Morek <matthew@madbit.co>
2019-11-19 14:24:12 +0000

    📖 Add MessageBoard component [ch42]

---
+++ src/components/MessageBoard/MessageBoard.stories.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import MessageBoard from '.';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+
+export default {
+  title: 'Elements|MessageBoard',
+  decorators: [withKnobs(), withA11y()]
+};
+
+export const standard = () => ({
+  components: { MessageBoard },
+  props: {
+    message: {
+      default: text('Message', 'Surfaces may be icy - please take extra care')
+    }
+  },
+  template: '<message-board :message="message" />'
+});
