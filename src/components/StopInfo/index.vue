07eab21fc279a6f8b9aeae7c706752adc4677eda
Matthew Morek <matthew@madbit.co>
2019-11-21 13:20:10 +0000

    💅 Make StopInfo sticky on scroll

--- src/components/StopInfo/index.vue
+++ src/components/StopInfo/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="stop-info">
-    <div class="stop-info--icon">
-      <app-icon width="52" height="52" />
+    <div class="stop-info--container">
+      <div class="stop-info--icon">
+        <app-icon width="52" height="52" />
+      </div>
+      <div>
+        <h2 class="stop-info--name">{{ stopLocation }}</h2>
+        <p class="stop-info--distance">{{ distanceToStop }} mls away</p>
+      </div>
     </div>
-    <div>
-      <h2 class="stop-info--name">{{ stopLocation }}</h2>
-      <p class="stop-info--distance">{{ distanceToStop }} mls away</p>
-    </div>
+    <div class="stop-info--bg"></div>
   </div>
 </template>
 
@@ -34,11 +37,30 @@ export default {
 
 <style lang="postcss">
 .stop-info {
-  display: flex;
-  align-items: center;
   width: 100%;
-  margin-top: 0.5rem;
-  margin-bottom: 0.5rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  position: sticky;
+  top: 0;
+
+  &--bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--app-bg-blur);
+    --webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    z-index: 0;
+  }
+
+  &--container {
+    position: relative;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+  }
 
   &--icon {
     margin-right: 0.25rem;
