ee90946365a4d842e4d3e1d19010059a19a22245
Matthew Morek <matthew@madbit.co>
2019-11-21 10:21:15 +0000

    🔨 Restructure template directories

---
+++ src/layout/AppWelcome/AppWelcome.stories.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import AppWelcome from '.';
+
+export default {
+  title: 'Templates|AppWelcome'
+};
+
+export const Default = () => ({
+  components: { AppWelcome },
+  template: '<app-welcome />'
+});
